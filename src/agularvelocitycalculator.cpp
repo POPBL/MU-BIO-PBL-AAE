706a91fd77dc53c9d32cf3072526ee9cc3b65357
Maryum Bibi <maryum.bibi@alumni.mondragon.edu>
2025-01-23 10:54:50 +0100

    Angular velocity calculated.

--- src/agularvelocitycalculator.cpp
+++ src/agularvelocitycalculator.cpp
@@ -1,6 +1,38 @@
 #include "angularvelocitycalculator.h"
 
 float computeMaximumAngularVelocity(const point_t* points, int numberOfPoints) {
-	//TODO 3. Compute the maximum angular velocity
-	return 0;
+	
+    if (numberOfPoints < 2)
+    {
+        printf("Not enough points for angular velocity calculation.\n");
+        return 0.0f;
+    }
+
+    float maxAngularVelocity = 0.0f;
+
+    for (int i = 1; i < numberOfPoints; i++)
+    {
+        double deltaTheta = points[i].theta - points[i - 1].theta;
+        double deltaT = points[i].t - points[i - 1].t;
+
+        printf("Point %d: deltaTheta=%f, deltaT=%f\n", i, deltaTheta, deltaT);
+
+        // Check if deltaT is sufficiently large to avoid errors
+        if (fabs(deltaT) > 1e-6)
+        {
+            float angularVelocity = fabs(deltaTheta / deltaT);
+            printf("Computed angular velocity: %f\n", angularVelocity);
+
+            if (angularVelocity > maxAngularVelocity)
+            {
+                maxAngularVelocity = angularVelocity;
+            }
+        }
+        else
+        {
+            printf("Skipped point %d due to small or zero deltaT.\n", i);
+        }
+    }
+
+    return maxAngularVelocity;
 }
